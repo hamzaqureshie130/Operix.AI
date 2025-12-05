751444db5aef7d5639deb5437f9fa0e98d07f17a
mudassar96 <mudassarmehdi96@gmail.com>
2025-12-05 23:46:24 +0500

    create baseDomainEntity

---
+++ Operix.Domain/Entities/Common/BaseDomainEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operix.Domain.Entities.Common
+{
+    public abstract class BaseDomainEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
